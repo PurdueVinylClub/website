ffb4f0cea6561f14664059e55eca8b1cb1c9775b
evanklein@bitbucket.com <evanklein@spensatech.com>
2018-12-27 21:31:51 -0500

    Initial commit to new github

--- src/app/services/general.service.ts
+++ src/app/services/general.service.ts
@@ -1,26 +1,23 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Credientals } from "../credentials.js";
 
 @Injectable()
 export class GeneralService {
     // A generic service to use throughout the app.
     // Provided in app.module.ts.
 
-    DISCOGS_AUTH_TOKEN = 'IIjaKnQGrAVYYGmHXSSZsAzgHVRNaRMfFocJBgxc';
-    GOOGLE_CAL_API_KEY = 'AIzaSyCOtAHDrc_odjH2l2M5FyDlwvmHMmUDTnM';
-    GOOGLE_CAL_CALENDAR_ID = 'purduevinylclub@gmail.com';
-
     constructor(private httpClient: HttpClient) {}
 
     getDiscogsClubCollection(callbackFunction: (data) => void) {
-        let url = 'https://api.discogs.com/users/PurdueVinylClub/collection/folders/0/releases?token=' + this.DISCOGS_AUTH_TOKEN;
+        let url = 'https://api.discogs.com/users/PurdueVinylClub/collection/folders/0/releases?token=' + Credientals.DISCOGS_AUTH_TOKEN;
         this.httpClient.get(url).subscribe((result) => {
             callbackFunction(result);
         });
     }
 
     getGoogleCalendarEvents(callbackFunction: (data) => void ) {
-        let url = 'https://www.googleapis.com/calendar/v3/calendars/' + this.GOOGLE_CAL_CALENDAR_ID + '/events?key=' + this.GOOGLE_CAL_API_KEY;
+        let url = 'https://www.googleapis.com/calendar/v3/calendars/' + Credientals.GOOGLE_CAL_CALENDAR_ID + '/events?key=' + Credientals.GOOGLE_CAL_API_KEY;
         this.httpClient.get(url).subscribe((result) => {
             // Have to wrap calendar events into new format so that Angular FullCalendar can process the data correctly.
             let events = result['items'];
