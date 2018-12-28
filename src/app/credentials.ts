3fd48ca73f84f673b4e032d91942d7644abe74f9
evanklein@bitbucket.com <evanklein@spensatech.com>
2018-12-28 12:28:41 -0500

    Evan deploying to Heroku @ Fri Dec 28 12:28:41 EST 2018

--- src/app/credentials.ts
+++ src/app/credentials.ts
@@ -1,5 +1,5 @@
-export class Credentials {
-    DISCOGS_AUTH_TOKEN = 'IIjaKnQGrAVYYGmHXSSZsAzgHVRNaRMfFocJBgxc';
-    GOOGLE_CAL_API_KEY = 'AIzaSyCOtAHDrc_odjH2l2M5FyDlwvmHMmUDTnM';
-    GOOGLE_CAL_CALENDAR_ID = 'purduevinylclub@gmail.com';
-}
+'use strict';
+
+export const DISCOGS_AUTH_TOKEN = 'IIjaKnQGrAVYYGmHXSSZsAzgHVRNaRMfFocJBgxc';
+export const GOOGLE_CAL_API_KEY = 'AIzaSyCOtAHDrc_odjH2l2M5FyDlwvmHMmUDTnM';
+export const GOOGLE_CAL_CALENDAR_ID = 'purduevinylclub@gmail.com';
--- src/app/services/general.service.ts
+++ src/app/services/general.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Credientals } from "../credentials.js";
+import * as Credentials from "../credentials";
 
 @Injectable()
 export class GeneralService {
@@ -10,14 +10,14 @@ export class GeneralService {
     constructor(private httpClient: HttpClient) {}
 
     getDiscogsClubCollection(callbackFunction: (data) => void) {
-        let url = 'https://api.discogs.com/users/PurdueVinylClub/collection/folders/0/releases?token=' + Credientals.DISCOGS_AUTH_TOKEN;
+        let url = 'https://api.discogs.com/users/PurdueVinylClub/collection/folders/0/releases?token=' + Credentials.DISCOGS_AUTH_TOKEN;
         this.httpClient.get(url).subscribe((result) => {
             callbackFunction(result);
         });
     }
 
     getGoogleCalendarEvents(callbackFunction: (data) => void ) {
-        let url = 'https://www.googleapis.com/calendar/v3/calendars/' + Credientals.GOOGLE_CAL_CALENDAR_ID + '/events?key=' + Credientals.GOOGLE_CAL_API_KEY;
+        let url = 'https://www.googleapis.com/calendar/v3/calendars/' + Credentials.GOOGLE_CAL_CALENDAR_ID + '/events?key=' + Credentials.GOOGLE_CAL_API_KEY;
         this.httpClient.get(url).subscribe((result) => {
             // Have to wrap calendar events into new format so that Angular FullCalendar can process the data correctly.
             let events = result['items'];
