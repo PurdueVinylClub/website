8f755a95711151c249050bff26a06b84d8693d2d
evanklein@bitbucket.com <evanklein@spensatech.com>
2018-12-28 12:17:22 -0500

    Evan deploying to Heroku @ Fri Dec 28 12:17:22 EST 2018

---
+++ src/app/credentials.js
@@ -0,0 +1,5 @@
+export class Credentials {
+    DISCOGS_AUTH_TOKEN = 'IIjaKnQGrAVYYGmHXSSZsAzgHVRNaRMfFocJBgxc';
+    GOOGLE_CAL_API_KEY = 'AIzaSyCOtAHDrc_odjH2l2M5FyDlwvmHMmUDTnM';
+    GOOGLE_CAL_CALENDAR_ID = 'purduevinylclub@gmail.com';
+}
