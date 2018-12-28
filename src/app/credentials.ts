6ec7b6ef84c8bd0625f9a2753c5232381a9e5d9f
Evan <klein57@purdue.edu>
2018-12-28 14:27:48 -0500

    Evan testing credentials (#2)
    * Testing permissions.
    
    * Evan deploying to Heroku @ Fri Dec 28 12:17:22 EST 2018
    
    * Evan deploying to Heroku @ Fri Dec 28 12:20:40 EST 2018
    
    * Evan deploying to Heroku @ Fri Dec 28 12:28:41 EST 2018
    
    * Evan deploying to Heroku @ Fri Dec 28 12:31:19 EST 2018
    
    * Evan deploying to Heroku @ Fri Dec 28 12:35:33 EST 2018
    
    * Testing credentials
    
    * Testing branching

---
+++ src/app/credentials.ts
@@ -0,0 +1,5 @@
+'use strict';
+
+export const DISCOGS_AUTH_TOKEN = 'IIjaKnQGrAVYYGmHXSSZsAzgHVRNaRMfFocJBgxc';
+export const GOOGLE_CAL_API_KEY = 'AIzaSyCOtAHDrc_odjH2l2M5FyDlwvmHMmUDTnM';
+export const GOOGLE_CAL_CALENDAR_ID = 'purduevinylclub@gmail.com';
